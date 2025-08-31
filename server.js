2d59a654054d14e0f41cce5d81d81749c0543f9d
Bharti152k2 <nidhi15sak@gmail.com>
2025-08-31 23:42:13 +0530

    origin correction on redirects added

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const http = require("http");
 const subscribeMail = require("./mail");
-
+const allowedOrigins = [
+  "http://localhost:5173/",
+  "https://mailsenderui.netlify.app/",
+];
 let server = http.createServer((req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle preflight request
+  if (req.method === "OPTIONS") {
+    res.writeHead(204);
+    res.end();
+    return;
+  }
 
   if (req.url === "/sendmail" && req.method === "POST") {
     req.on("data", (body) => {
